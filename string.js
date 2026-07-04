409d9b7dae5f93c65a227141a74eb7343a5929f5
laravelrytclick-gif <laravel.rytclick@gmail.com>
2026-07-04 20:15:26 +0530

    duplicate-array-problem

--- string.js
+++ string.js
@@ -1,4 +1,4 @@
-const arr = [1,2,3,4,5,6,7,8,9,10];
+const kok = [1,2,3,4,5,3,6,7,8,9,10];
 function reverseArray(arr){
     let start = 0
     let end = arr.length - 1
@@ -17,4 +17,51 @@ function swip(arr , start ,end){
 
 }
 
-console.log(reverseArray(arr))
+// function removeDup(arr){
+//     const result = []
+
+//     for(const num of arr){
+//         if(!result.includes(num)){
+//             result.push(num)
+//         }
+//     }
+
+//     return result
+// }
+
+// function removeDuplicates(arr) {
+//     if (arr.length === 0) return [];
+
+//     let i = 0;
+
+//     for (let j = 1; j < arr.length; j++) {
+//         if (arr[i] !== arr[j]) {
+//             i++;
+//             arr[i] = arr[j];
+//         }
+//     }
+
+//     return arr.slice(0, i + 1);
+// }
+// const kok = [1, 1, 2, 2, 3, 4, 4, 5];
+
+function removeDuplicates(arr) {
+    if (arr.length === 0) return [];
+
+    let i = 0;
+
+    for (let j = 1; j < arr.length; j++) {
+        if (arr[i] !== arr[j]) {
+            i++;
+            arr[i] = arr[j];
+        }
+    }
+
+    return arr.slice(0, i + 1);
+}
+
+console.log(removeDuplicates(kok));
+
+// console.log(removeDuplicates([1, 1, 2, 2, 3, 4, 4, 5]));
+// console.log(removeDuplicates(arr))
+// console.log(reverseArray(arr))
