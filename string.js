7a4632a704d0b562c8e4870785627c2886009021
laravelrytclick-gif <laravel.rytclick@gmail.com>
2026-07-05 01:07:58 +0530

    add

--- string.js
+++ string.js
@@ -60,6 +60,23 @@ function removeDuplicates(arr) {
     return arr.slice(0, i + 1);
 }
 
+function twoSum(arr,target){
+    
+    const map = new Map();
+    for(let i = 0 ; i<arr.length;i++){
+        const diff = target-arr[i];
+        
+        
+        if(map.has(diff)){
+            return [map.get(diff),i]
+        }
+        
+        map.set(arr[i],i)
+    }
+     return [];
+   
+    
+}
 console.log(removeDuplicates(kok));
 
 // console.log(removeDuplicates([1, 1, 2, 2, 3, 4, 4, 5]));
