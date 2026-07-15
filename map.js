904a54eeaa1c62b5a6ead253ea86debb18b0c59a
Gaurav Rathore <Vimleshrathor1984@gmail.com>
2026-07-15 23:52:34 +0530

    add

--- map.js
+++ map.js
@@ -1,13 +1,23 @@
 const arr = [1,2,3,4,5]
 
-function twoSum(arr,target){
-    const map = new Map()
-    for(let i = 0 ; i <arr.length-1; i++){
-        let diff = target-arr[i]
-        if(map.has(diff)){
-            return [arr[i],i]
+function twoSum(arr, target) {
+    const map = new Map();
+
+    for (let i = 0; i < arr.length; i++) {
+        const diff = target - arr[i];
+
+        if (map.has(diff)) {
+            return [map.get(diff), i]; // return indices
         }
+
+        map.set(arr[i], i);
     }
-    return map.set(arr[i],i)
+
+    return [];
 }
-console.log(twoSum(arr))
+
+const arr = [2, 7, 11, 15];
+const target = 9;
+
+console.log(twoSum(arr, target)); // [0, 1]
+// console.log(twoSum(arr))
