e686c499d4af5d865f43698cfe06cb5dee51f51d
Gaurav Rathore <vimleshrathor1984@gmail.com>
2026-07-24 23:20:42 +0530

    setZeromatrixx

--- array.js
+++ array.js
@@ -191,6 +191,37 @@ function trap(height) {
     return water;
 }
 
+function setZeroes(matrix) {
+    let m = matrix.length;
+    let n = matrix[0].length;
+
+    let col0 = 1;
+
+    // Mark rows and cols
+    for (let i = 0; i < m; i++) {
+        if (matrix[i][0] === 0) col0 = 0;
+
+        for (let j = 1; j < n; j++) {
+            if (matrix[i][j] === 0) {
+                matrix[i][0] = 0;
+                matrix[0][j] = 0;
+            }
+        }
+    }
+
+    // Fill from bottom-right
+    for (let i = m - 1; i >= 0; i--) {
+        for (let j = n - 1; j >= 1; j--) {
+            if (matrix[i][0] === 0 || matrix[0][j] === 0) {
+                matrix[i][j] = 0;
+            }
+        }
+
+        if (col0 === 0) {
+            matrix[i][0] = 0;
+        }
+    }
+}
 
 
 // console.log(twoSum([2,7,11,15],9))
