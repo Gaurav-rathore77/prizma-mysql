f600fbaab4976892d26d57e6b2976c3501e4508f
Gaurav Rathore <vimleshrathor1984@gmail.com>
2026-07-24 23:22:00 +0530

    spiralOrder

--- array.js
+++ array.js
@@ -223,6 +223,49 @@ function setZeroes(matrix) {
     }
 }
 
+function spiralOrder(matrix) {
+    let result = [];
+
+    let top = 0;
+    let bottom = matrix.length - 1;
+
+    let left = 0;
+    let right = matrix[0].length - 1;
+
+    while (top <= bottom && left <= right) {
+
+        // Left -> Right
+        for (let i = left; i <= right; i++) {
+            result.push(matrix[top][i]);
+        }
+        top++;
+
+        // Top -> Bottom
+        for (let i = top; i <= bottom; i++) {
+            result.push(matrix[i][right]);
+        }
+        right--;
+
+        // Right -> Left
+        if (top <= bottom) {
+            for (let i = right; i >= left; i--) {
+                result.push(matrix[bottom][i]);
+            }
+            bottom--;
+        }
+
+        // Bottom -> Top
+        if (left <= right) {
+            for (let i = bottom; i >= top; i--) {
+                result.push(matrix[i][left]);
+            }
+            left++;
+        }
+    }
+
+    return result;
+}
+
 
 // console.log(twoSum([2,7,11,15],9))
 // console.log(maxSum([1,2,3,-1,5]))
