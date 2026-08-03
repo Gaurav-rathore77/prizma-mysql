3d77d83273e9ff8fad1fe98eeadeb7c0162d2325
Gaurav Rathore <vimleshrathor1984@gmail.com>
2026-08-04 02:15:45 +0530

    firstvowel

--- string.js
+++ string.js
@@ -181,6 +181,18 @@ function reverseString(str) {
 
     return string;
 }
+function reverseString1(str) {
+    let left = 0;
+    let right = str.length - 1;
+
+    while (left < right) {
+        [str[left], str[right]] = [str[right], str[left]];
+        left++;
+        right--;
+    }
+
+    return str;
+}
 
 function palindrome(str) {
     let first = 0;
