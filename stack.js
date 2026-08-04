99b9782618722f1b713b5434c195fe585df65fd9
Gaurav Rathore <vimleshrathor1984@gmail.com>
2026-08-04 16:06:17 +0530

    dailytempraturese

--- stack.js
+++ stack.js
@@ -112,4 +112,28 @@ function nextGreaterElement(nums1, nums2) {
     return nums1.map(num => map.get(num));
 } 
 
+function dailyTemperatures(temperatures) {
+    const n = temperatures.length;
+    const ans = new Array(n).fill(0);
+    const stack = [];
+
+    for (let i = n - 1; i >= 0; i--) {
+
+        while (
+            stack.length &&
+            temperatures[stack[stack.length - 1]] <= temperatures[i]
+        ) {
+            stack.pop();
+        }
+
+        if (stack.length) {
+            ans[i] = stack[stack.length - 1] - i;
+        }
+
+        stack.push(i);
+    }
+
+    return ans;
+}
+
 console.log(nextGreaterElement([4,1,2], [1,3,4,2]));
