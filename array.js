be524f4c95e465d031d7b7c4e8491143f8655839
Gaurav Rathore <vimleshrathor1984@gmail.com>
2026-08-05 17:53:45 +0530

    next

--- array.js
+++ array.js
@@ -266,6 +266,41 @@ function spiralOrder(matrix) {
     return result;
 }
 
+function nextPermutation(nums) {
+    let i = nums.length - 2;
+
+    // Step 1: Find pivot
+    while (i >= 0 && nums[i] >= nums[i + 1]) {
+        i--;
+    }
+
+    // Step 2: Find next greater element and swap
+    if (i >= 0) {
+        let j = nums.length - 1;
+
+        while (nums[j] <= nums[i]) {
+            j--;
+        }
+
+        [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+
+    // Step 3: Reverse the suffix
+    reverse(nums, i + 1, nums.length - 1);
+}
+
+function reverse(nums, left, right) {
+    while (left < right) {
+        [nums[left], nums[right]] = [nums[right], nums[left]];
+        left++;
+        right--;
+    }
+}
+
+// Example
+let nums = [1, 2, 3];
+nextPermutation(nums);
+console.log(nums); // [1, 3, 2]
 function maxSlidingWindow(nums, k) {
 
     let deque = [];
