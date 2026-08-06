a044868a891f74e98259ab455af60b225a6421bf
Gaurav Rathore <vimleshrathor1984@gmail.com>
2026-08-07 00:58:51 +0530

    searchInsert

--- search.js
+++ search.js
@@ -17,5 +17,41 @@ function binarySearch(arr, target) {
     return -1;
 }
 
+function searchInsert(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return left;
+}function searchInsert(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return left;
+}
+
 const arr = [2, 4, 6, 8, 10, 12];
 console.log(binarySearch(arr, 8)); // 3
