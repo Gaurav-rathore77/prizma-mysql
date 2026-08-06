f800cf91818b3524376edb5799fb766b0d0a9fc3
Gaurav Rathore <vimleshrathor1984@gmail.com>
2026-08-07 00:27:54 +0530

    binarysearch

---
+++ search.js
@@ -0,0 +1,21 @@
+function binarySearch(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor(left + (right - left) / 2);
+
+        if (arr[mid] === target) {
+            return mid;
+        } else if (arr[mid] > target) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    return -1;
+}
+
+const arr = [2, 4, 6, 8, 10, 12];
+console.log(binarySearch(arr, 8)); // 3
