e3da87b23810d1bfb1ef0d5e753be933743bae7e
Gaurav Rathore <vimleshrathor1984@gmail.com>
2026-08-06 16:55:28 +0530

    invertedpramid

--- patterns.js
+++ patterns.js
@@ -7,4 +7,26 @@ function sqPrint(n){
         console.log(row)
     }
 }
+
+function piraidPrint(n){
+    for(let i = 0 ; i< n ; i ++){
+        let row = ""
+        for(let j  = 0 ; j < i; j++){
+            row+="*"
+        }
+        console.log(row)
+    }
+}
+
+function invertPiramidPrint(n){
+    for(let i =n ; i>= 0; i --){
+        let row = ""
+        for(let j  = 0 ; j < i; j++){
+            row+="*"
+        }
+        console.log(row)
+    }
+}
+invertPiramidPrint(5)
+piraidPrint(5)
 sqPrint(5)
