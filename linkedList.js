09dc8bd570b6b68a2e50bc9485ccd2ba0bd297b0
Gaurav Rathore <vimleshrathor1984@gmail.com>
2026-08-10 23:09:53 +0530

    middleNode

--- linkedList.js
+++ linkedList.js
@@ -24,3 +24,15 @@ function reverseList(head) {
 
     return newHead;
 }
+
+function middleNode(head) {
+    let slow = head;
+    let fast = head;
+
+    while (fast !== null && fast.next !== null) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    return slow;
+}
