564f5b36fbcdebc17b632146e30fe2dc4f1cbff7
Gaurav Rathore <vimleshrathor1984@gmail.com>
2026-08-13 23:49:36 +0530

    quicksort

--- sort.js
+++ sort.js
@@ -103,6 +103,30 @@ function merge(left, right) {
   return result;
 }
 
+
+function quickSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const pivot = arr[arr.length - 1];
+
+  const left = [];
+  const right = [];
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+
+  return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+console.log(quickSort([5, 3, 8, 4, 2, 7, 1, 6]));
+// [1, 2, 3, 4, 5, 6, 7, 8]
 console.log(mergeSort([5, 3, 8, 4, 2, 7, 1, 6]));
 // [1, 2, 3, 4, 5, 6, 7, 8]
 console.log(insertionSort([5, 3, 8, 1, 2]));
