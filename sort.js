b2c7b74845b62258816e0f7fd0b930bb8bcaf20c
Gaurav Rathore <vimleshrathor1984@gmail.com>
2026-08-13 23:53:48 +0530

    sort012

--- sort.js
+++ sort.js
@@ -233,6 +233,34 @@ function heapify(arr, n, i) {
     }
 }
 
+
+function sort012(arr) {
+    let low = 0;
+    let mid = 0;
+    let high = arr.length - 1;
+
+    while (mid <= high) {
+
+        if (arr[mid] === 0) {
+            [arr[low], arr[mid]] = [arr[mid], arr[low]];
+            low++;
+            mid++;
+        }
+
+        else if (arr[mid] === 1) {
+            mid++;
+        }
+
+        else {
+            [arr[mid], arr[high]] = [arr[high], arr[mid]];
+            high--;
+        }
+    }
+
+    return arr;
+}
+
+console.log(sort012([2, 0, 2, 1, 1, 0]));
 console.log(heapSort([4, 10, 3, 5, 1]));
 console.log(radixSort([170, 45, 75, 90, 802, 24, 2, 66]));
 console.log(countingSort([4, 2, 2, 8, 3, 3, 1]));
