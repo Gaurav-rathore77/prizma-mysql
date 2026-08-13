59914cc394b4f1ebde79458a4e51d255ac7d748e
Gaurav Rathore <vimleshrathor1984@gmail.com>
2026-08-13 23:48:12 +0530

    insertionsort

--- sort.js
+++ sort.js
@@ -37,6 +37,25 @@ function selectionSort(arr) {
     return arr;
 }
 
+
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+
+        let key = arr[i];
+        let j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+
+    return arr;
+}
+
+console.log(insertionSort([5, 3, 8, 1, 2]));
 console.log(selectionSort([5, 3, 8, 1, 2]));
 console.log(bubbleSort([5, 3, 8, 4, 2]));
 // [2, 3, 4, 5, 8]
