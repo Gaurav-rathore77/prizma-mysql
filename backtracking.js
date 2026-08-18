d8809365de0cab7e5fb30a6918187ad4e38cb9c9
Gaurav Rathore <vimleshrathor1984@gmail.com>
2026-08-18 23:49:58 +0530

    permute

--- backtracking.js
+++ backtracking.js
@@ -22,4 +22,37 @@ function subsets(nums) {
     return result;
 }
 
+function permute(nums) {
+    const result = [];
+    const path = [];
+    const used = new Array(nums.length).fill(false);
+
+    function backtrack() {
+        // Complete permutation
+        if (path.length === nums.length) {
+            result.push([...path]);
+            return;
+        }
+
+        for (let i = 0; i < nums.length; i++) {
+            if (used[i]) continue;
+
+            // Choose
+            used[i] = true;
+            path.push(nums[i]);
+
+            // Explore
+            backtrack();
+
+            // Undo
+            path.pop();
+            used[i] = false;
+        }
+    }
+
+    backtrack();
+    return result;
+}
+
+console.log(permute([1, 2, 3]));
 console.log(subsets([1, 2, 3]));
