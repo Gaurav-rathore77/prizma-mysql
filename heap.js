e2a79e63823614f0901f313709837db6d9d5d267
Gaurav Rathore <vimleshrathor1984@gmail.com>
2026-08-19 23:07:30 +0530

    maxHeaap

--- heap.js
+++ heap.js
@@ -85,3 +85,89 @@ class MinHeap {
     }
 }
 
+class MaxHeap {
+    constructor() {
+        this.heap = [];
+    }
+
+    size() {
+        return this.heap.length;
+    }
+
+    peek() {
+        return this.heap[0];
+    }
+
+    insert(value) {
+        this.heap.push(value);
+        this.bubbleUp();
+    }
+
+    bubbleUp() {
+        let index = this.heap.length - 1;
+
+        while (index > 0) {
+            let parent = Math.floor((index - 1) / 2);
+
+            if (this.heap[parent] >= this.heap[index]) {
+                break;
+            }
+
+            [this.heap[parent], this.heap[index]] =
+            [this.heap[index], this.heap[parent]];
+
+            index = parent;
+        }
+    }
+
+    remove() {
+        if (this.heap.length === 0) return null;
+
+        if (this.heap.length === 1) {
+            return this.heap.pop();
+        }
+
+        const max = this.heap[0];
+
+        this.heap[0] = this.heap.pop();
+
+        this.bubbleDown();
+
+        return max;
+    }
+
+    bubbleDown() {
+        let index = 0;
+        const n = this.heap.length;
+
+        while (true) {
+            let left = 2 * index + 1;
+            let right = 2 * index + 2;
+
+            let largest = index;
+
+            if (
+                left < n &&
+                this.heap[left] > this.heap[largest]
+            ) {
+                largest = left;
+            }
+
+            if (
+                right < n &&
+                this.heap[right] > this.heap[largest]
+            ) {
+                largest = right;
+            }
+
+            if (largest === index) {
+                break;
+            }
+
+            [this.heap[index], this.heap[largest]] =
+            [this.heap[largest], this.heap[index]];
+
+            index = largest;
+        }
+    }
+}
