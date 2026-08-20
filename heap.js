d5542835a01302ad5892d3b346b108c283f0fde3
Gaurav Rathore <vimleshrathor1984@gmail.com>
2026-08-20 23:45:28 +0530

    findkthlargest

--- heap.js
+++ heap.js
@@ -297,3 +297,34 @@ function heapSort(arr) {
 
     return arr;
 }
+
+function findKthLargest(nums, k) {
+    const target = nums.length - k;
+
+    function quickSelect(left, right) {
+        const pivot = nums[right];
+        let p = left;
+
+        // Partition: elements smaller than pivot go left
+        for (let i = left; i < right; i++) {
+            if (nums[i] <= pivot) {
+                [nums[i], nums[p]] = [nums[p], nums[i]];
+                p++;
+            }
+        }
+
+        [nums[p], nums[right]] = [nums[right], nums[p]];
+
+        if (p === target) {
+            return nums[p];
+        }
+
+        if (p < target) {
+            return quickSelect(p + 1, right);
+        }
+
+        return quickSelect(left, p - 1);
+    }
+
+    return quickSelect(0, nums.length - 1);
+}
