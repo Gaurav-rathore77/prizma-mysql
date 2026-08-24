a321495450fd38b493198adb3201680dcf0cacd7
Gaurav Rathore <vimleshrathor1984@gmail.com>
2026-08-24 23:25:20 +0530

    maxsliding

--- queue.js
+++ queue.js
@@ -74,3 +74,34 @@ class MyStack {
         return this.q.length === 0;
     }
 }
+
+function maxSlidingWindow(nums, k) {
+    const deque = [];
+    const result = [];
+
+    for (let i = 0; i < nums.length; i++) {
+
+        // 1. Remove elements outside the window
+        while (deque.length && deque[0] <= i - k) {
+            deque.shift();
+        }
+
+        // 2. Remove smaller elements
+        while (
+            deque.length &&
+            nums[deque[deque.length - 1]] <= nums[i]
+        ) {
+            deque.pop();
+        }
+
+        // 3. Add current index
+        deque.push(i);
+
+        // 4. Window is ready
+        if (i >= k - 1) {
+            result.push(nums[deque[0]]);
+        }
+    }
+
+    return result;
+}
