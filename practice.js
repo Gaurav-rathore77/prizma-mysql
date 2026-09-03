82d8ee43dbb0a42123df03046430e82aea4b52d0
Gaurav Rathore <vimleshrathor1984@gmail.com>
2026-09-03 23:31:29 +0530

    add

--- practice.js
+++ practice.js
@@ -208,3 +208,21 @@ function threeSum(nums) {
 
     return result;
 }
+function binarySearch(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor(left + (right - left) / 2);
+
+        if (arr[mid] === target) {
+            return mid;
+        } else if (arr[mid] > target) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    return -1;
+}
