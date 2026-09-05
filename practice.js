796985fc680dc1a6b32c6bbabff5a216bc6ff388
Gaurav Rathore <vimleshrathor1984@gmail.com>
2026-09-05 23:36:10 +0530

    add

--- practice.js
+++ practice.js
@@ -277,3 +277,17 @@ function searchInsert(nums, target) {
 
     return left;
 }
+
+function largestNumber(nums) {
+    nums = nums.map(String);
+
+    nums.sort((a, b) => {
+        return (b + a).localeCompare(a + b);
+    });
+
+    if (nums[0] === "0") {
+        return "0";
+    }
+
+    return nums.join("");
+}
